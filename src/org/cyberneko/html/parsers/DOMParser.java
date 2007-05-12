78d7768dce492c6a9197137f7dfc2912dc5e448d
andyc2 <andyc2@8a2d9b43-3d30-0410-9efd-cf3042b7273d>
2007-05-12 05:56:51 +0000

    nekohtml-0.3.3

---
+++ src/org/cyberneko/html/parsers/DOMParser.java
@@ -0,0 +1,53 @@
+/* 
+ * (C) Copyright 2002, Andy Clark.  All rights reserved.
+ *
+ * This file is distributed under an Apache style license. Please
+ * refer to the LICENSE file for specific details.
+ */
+
+package org.cyberneko.html.parsers;
+
+import org.cyberneko.html.HTMLConfiguration;
+
+/**
+ * A DOM parser for HTML documents.
+ *
+ * @author Andy Clark
+ */
+public class DOMParser
+    /***/
+    extends org.apache.xerces.parsers.DOMParser {
+    /***
+    // NOTE: It would be better to extend from AbstractDOMParser but
+    //       most users will find it easier if the API is just like the
+    //       Xerces DOM parser. By extending directly from DOMParser,
+    //       users can register SAX error handlers, entity resolvers,
+    //       and the like. -Ac
+    extends org.apache.xerces.parsers.AbstractDOMParser {
+    /***/
+
+    //
+    // Constructors
+    //
+
+    /** Default constructor. */
+    public DOMParser() {
+        super(new HTMLConfiguration());
+        /*** extending DOMParser ***/
+        try {
+            setProperty("http://apache.org/xml/properties/dom/document-class-name",
+                                       "org.apache.html.dom.HTMLDocumentImpl");
+        }
+        catch (org.xml.sax.SAXNotRecognizedException e) {
+            throw new RuntimeException("http://apache.org/xml/properties/dom/document-class-name property not recognized");
+        }
+        catch (org.xml.sax.SAXNotSupportedException e) {
+            throw new RuntimeException("http://apache.org/xml/properties/dom/document-class-name property not supported");
+        }
+        /*** extending AbstractDOMParser ***
+        fConfiguration.setProperty("http://apache.org/xml/properties/dom/document-class-name",
+                                   "org.apache.html.dom.HTMLDocumentImpl");
+        /***/
+    } // <init>()
+
+} // class DOMParser
