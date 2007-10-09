b3c386aff9ef8124038b7962275a26e3c2429460
andyc2 <andyc2@8a2d9b43-3d30-0410-9efd-cf3042b7273d>
2007-10-09 07:07:08 +0000

    Updated license to Apache 2.0.

--- src/test/Writer.java
+++ src/test/Writer.java
@@ -1,10 +1,19 @@
 /* 
- * (C) Copyright 2002-2005, Andy Clark.  All rights reserved.
+ * Copyright 2002-2007 Andy Clark
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This file is distributed under an Apache style license. Please
- * refer to the LICENSE file for specific details.
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
- 
+
 package test;
 
 import org.cyberneko.html.filters.DefaultFilter;
