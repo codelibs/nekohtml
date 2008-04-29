3c28a4b53eaea5855fe10890211129c31e657a87
mguillem <mguillem@8a2d9b43-3d30-0410-9efd-cf3042b7273d>
2008-04-29 08:58:27 +0000

    fixed bug #1951703 (isEncodingCompatible throws UnsupportedOperationException when default encoding is JISAutoDetect, patch from Tatsuhiko Miyabe)

--- src/org/cyberneko/html/HTMLScanner.java
+++ src/org/cyberneko/html/HTMLScanner.java
@@ -3386,19 +3386,8 @@ public class HTMLScanner
 		final String reference = "<html><head><meta http-equiv=\"Content-Type\" content=\"text/html;charset=";
 		try {
 			final byte[] bytesEncoding1 = reference.getBytes(encoding1);
-			final byte[] bytesEncoding2 = reference.getBytes(encoding2);
-			if (bytesEncoding1.length != bytesEncoding2.length) {
-				return false;
-			}
-			else {
-				for (int i=0; i<bytesEncoding1.length; ++i) {
-					if (bytesEncoding1[i] != bytesEncoding2[i]) {
-						return false;
-					}
-				}
-			}
-
-			return true;
+			final String referenceWithEncoding2 = new String(bytesEncoding1, encoding2);
+			return reference.equals(referenceWithEncoding2);
 		}
 		catch (final UnsupportedEncodingException e) {
 			return false;
